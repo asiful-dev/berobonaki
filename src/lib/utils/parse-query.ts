ecb80957e72b4e05654f24da68e83add1fd67bb7
asiful-dev <awful872@gmail.com>
2026-07-14 23:18:32 +0600

    feat: enhance weather API with validation for query parameters and error handling

--- src/lib/utils/parse-query.ts
+++ src/lib/utils/parse-query.ts
@@ -2,7 +2,7 @@ export function getQueryParams(url: string) {
   const { searchParams } = new URL(url);
 
   return {
-    lat: searchParams.get("lat"),
-    lon: searchParams.get("lon"),
+    lat: searchParams.get("lat") ?? undefined,
+    lon: searchParams.get("lon") ?? undefined,
   };
 }
